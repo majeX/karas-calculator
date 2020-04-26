6797b98901566a72e7ae9aa76a58b2f2b5476e59
Ruslan S <majex.exe@gmail.com>
2020-04-26 16:40:31 +0300

    Updates

---
+++ precache-manifest.39a5d0dca4668aa7556ced07b3479251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45fcc3730bd70c02bc8433b59c635c1",
+    "url": "/karas-calculator/index.html"
+  },
+  {
+    "revision": "6bd1942f265d9d687e3b",
+    "url": "/karas-calculator/static/css/main.526fbfe6.chunk.css"
+  },
+  {
+    "revision": "df40d930903b35247020",
+    "url": "/karas-calculator/static/js/2.e186053e.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/karas-calculator/static/js/2.e186053e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd1942f265d9d687e3b",
+    "url": "/karas-calculator/static/js/main.a37c410d.chunk.js"
+  },
+  {
+    "revision": "51257865e15c62821f7c",
+    "url": "/karas-calculator/static/js/runtime-main.87403b49.js"
+  },
+  {
+    "revision": "58446c0d100a4519192193bcfbc6e9b5",
+    "url": "/karas-calculator/static/media/karas.58446c0d.svg"
+  }
+]);
