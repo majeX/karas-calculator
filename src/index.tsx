d8bb4d1c8e448be76ca1109c33d333c0e50deda4
Ruslan S <majex.exe@gmail.com>
2020-08-05 23:21:13 +0300

    Add Farewell

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,25 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Changelog from './Changelog/Changelog';
-import Clanquest from './Clanquest/Clanquest';
+// import Changelog from './Changelog/Changelog';
+// import Clanquest from './Clanquest/Clanquest';
+import Farewell from './Farewell/Farewell';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Switch>
-        <Route path="/changelog">
-          <Changelog />
-        </Route>
-        <Route path="/clanquest">
-          <Clanquest />
-        </Route>
+        {/*<Route path="/changelog">*/}
+        {/*  <Changelog />*/}
+        {/*</Route>*/}
+        {/*<Route path="/clanquest">*/}
+        {/*  <Clanquest />*/}
+        {/*</Route>*/}
+        {/*<Route path="/">*/}
+        {/*  <App />*/}
+        {/*</Route>*/}
         <Route path="/">
-          <App />
+          <Farewell />
         </Route>
       </Switch>
     </HashRouter>
